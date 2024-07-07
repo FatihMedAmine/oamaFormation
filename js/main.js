f155ff030f0fa3332594183f4017169d8594d93a
FatihMedAmine <fatihmohamedamin@gmail.com>
2024-07-07 21:25:33 +0100

    page login finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const sections = document.querySelectorAll("section");
 
+window.onscroll = function () {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 250;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + 300 + height) {
+      sec.classList.add("show-animation");
+    }
+  });
+};
